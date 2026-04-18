7336b6b1ff55eac312056d2f5940afdf6b3196ff
Qingyun Li <qingy2019@outlook.com>
2026-04-18 12:02:45 -0700

    removed old pointwavelet code

--- benchmark/points/pointwavelet/__init__.py
+++
@@ -1,9 +0,0 @@
-from .models.pointwavelet_cls import PointWaveletClassifier, PointWaveletClsConfig
-from .models.pointwavelet_partseg import PointWaveletPartSeg, PointWaveletPartSegConfig
-
-__all__ = [
-    "PointWaveletClassifier",
-    "PointWaveletClsConfig",
-    "PointWaveletPartSeg",
-    "PointWaveletPartSegConfig",
-]
